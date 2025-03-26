d65d1a47a4ecc2a9972dae6378479d7a5fae1665
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-03-26 17:20:43 -0500

    project creation

---
+++ automation_project/src/main/java/org/portfolio/App.java
@@ -0,0 +1,13 @@
+package org.portfolio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
